57d270f77f52fa5ba22376a4aadef138a42fb10f
Kaeden1199 <76447743+Kaeden1199@users.noreply.github.com>
2021-04-28 13:43:32 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -19,23 +19,14 @@ function modelLoaded(){
     console.log(' model Loaded ');
 
 }
-function check(){
-    img= document.getElementById('captured_image');
-    classifier.classify(img, got_result);
+function check() { img = document.getElementById('captured_image'); 
+classifier.classify(img, gotResult); }
     
-    
-}
-function got_result(error, results){
-if(error){
 
-    console.error(error);
-} else{
+function gotResult(error, results) { if (error) { console.error(error); } else {
     console.log(results);
-    document.getElementById("result_object_name").innerHTML= results[0].label;
-    document.getElementById("result_object_accuracy").innerHTML= results[0].confiddence.toFixed(3);
-    
-
-    
+     document.getElementById("result_object_name").innerHTML = results[0].label;
+      document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
 
 }
 }
